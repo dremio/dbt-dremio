178a5e78a58444b54b25790bc0024ab23cdc6fe2
Jared LaRue <jared.larue@dremio.com>
2022-09-28 06:39:55 +0000

    Wired POST login, POST sql, GET job_status

--- dbt/adapters/dremio/api/__init__.py
+++ dbt/adapters/dremio/api/__init__.py
@@ -1,2 +1,4 @@
 ## __init__.py
 from .basic import login
+from .endpoints import sql_endpoint
+from .endpoints import job_status
--- dbt/adapters/dremio/api/error.py
+++ dbt/adapters/dremio/api/error.py
@@ -35,10 +35,10 @@ class DremioException(Exception):
 }
 """
 
-    def __init__(self, msg, original_exception, response=None):
-        super(DremioException, self).__init__(msg + (": %s" % original_exception))
-        self.original_exception = original_exception
-        self.response = response
+def __init__(self, msg, original_exception, response=None):
+    super(DremioException, self).__init__(msg + (": %s" % original_exception))
+    self.original_exception = original_exception
+    self.response = response
 
 
 class DremioUnauthorizedException(DremioException):
