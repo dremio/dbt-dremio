c773c20ee9cf755af5ea96acf715e70a755d2a6f
ArgusLi <argus.li@dremio.com>
2022-10-04 16:41:49 -0700

    Create api_get_response() which uses rest api instead of get_response()'s pyodbc.
    get_response() is treated as an interface, with all of the internal code being commented out and returning api_get_response(). This was done to ensure the logic works correctly without pyodbc.

--- dbt/adapters/dremio/connections.py
+++ dbt/adapters/dremio/connections.py
@@ -128,8 +128,6 @@ class DremioConnectionManager(SQLConnectionManager):
 
     @classmethod
     def open(cls, connection):
-        #breakpoint()
-
         if connection.state == 'open':
             logger.debug('Connection is already open, skipping open.')
             return connection
@@ -235,37 +233,13 @@ class DremioConnectionManager(SQLConnectionManager):
 
     @classmethod
     def get_response(cls, connection, cursor: pyodbc.Cursor, job_id) -> AdapterResponse:
-        ## keep checking job status until status is one of COMPLETE, CANCELLED, FAILED or METADATA
-        ## then call endpoints.job_results -> payload is schema and rows (unlikey to use offset as there shouldn't be too many rows)
-        ## mapr job results to cursor
-
-        token = connection.credentials.token
-        base_url = _build_base_url(connection.credentials)
-        last_job_state = ""
-        job_state = job_status(token, base_url, job_id, ssl_verify=True)["jobState"]
-
-        while True:
-            if job_state != last_job_state:
-                logger.debug(f"Job State = {job_state}")
-            if job_state == "COMPLETED" or job_state == "CANCELLED" or job_state == "FAILED":
-                break
-            last_job_state = job_state
-            job_state = job_status(token, base_url, job_id, ssl_verify=True)["jobState"]
-        
-        logger.debug(f"Job_id: {job_id}")
-        json_payload = job_results(token, base_url, job_id, offset=0, limit=100, ssl_verify=True)
-        # debug
-        logger.debug("Write JSON")
-        with open('dremio.connections.json', 'a') as fp:
-            fp.write(f"\nJob_id: {job_id}\n")
-            json.dump(json_payload, fp, indent=4)
-
-        rows = cursor.rowcount
-        message = 'OK' if rows == -1 else str(rows)
-        return AdapterResponse(
-            _message=message,
-            rows_affected=rows
-        )
+        #rows = cursor.rowcount
+        #message = 'OK' if rows == -1 else str(rows)
+        #return AdapterResponse(
+        #    _message=message,
+        #    rows_affected=rows
+        #)
+        return api_get_response(connection, job_id)
 
     def execute(
         self, sql: str, auto_begin: bool = False, fetch: bool = False
@@ -296,7 +270,44 @@ class DremioConnectionManager(SQLConnectionManager):
 def _build_base_url(credentials : DremioCredentials) -> str:
     return "http://{host}:{port}".format(host=credentials.host, port=credentials.rest_api_port)
 
+def api_get_response(connection, job_id) -> AdapterResponse:
+    ## keep checking job status until status is one of COMPLETE, CANCELLED or FAILED
+    ## map job results to AdapterResponse
+    token = connection.credentials.token
+    base_url = _build_base_url(connection.credentials)
+    last_job_state = ""
+    job_status_response = job_status(token, base_url, job_id, ssl_verify=True)
+    job_status_state = job_status_response["jobState"]
+
+    while True:
+        if job_status_state != last_job_state:
+            logger.debug(f"Job State = {job_status_state}")
+        if job_status_state == "COMPLETED":
+            message = job_status_state
+            break
+        elif job_status_state == "CANCELLED" or job_status_state == "FAILED":
+            message = job_status_state + ": " + job_status_response["errorMessage"]
+            break
+        last_job_state = job_status_state
+        job_status_response = job_status(token, base_url, job_id, ssl_verify=True)
+        job_status_state = job_status_response["jobState"]
+
+    #this is done as job status does not return a rowCount if there are no rows affected (even in completed job_state)
+    #pyodbc Cursor documentation states "[rowCount] is -1 if no SQL has been executed or if the number of rows is unknown.
+    # Note that it is not uncommon for databases to report -1 immediately after a SQL select statement for performance reasons."
+    if "rowCount" not in job_status_response:
+        rows = -1
+        logger.debug("rowCount does not exist in job_status payload")
+    else:
+        rows = job_status_response["rowCount"]
+
+    return AdapterResponse(
+        _message = message,
+        rows_affected = rows
+    )
+
 def get_result_from_api(connection, job_id) -> agate.Table:
+    
     token = connection.credentials.token
     base_url = _build_base_url(connection.credentials)
     json_payload = job_results(token, base_url, job_id, offset=0, limit=100, ssl_verify=True)["rows"]
