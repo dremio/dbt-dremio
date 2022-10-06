aca720b8b62c5536de88886ca562ecc88ac2a51b
Jared LaRue <jared.larue@dremio.com>
2022-10-06 04:11:26 +0000

    Add profile template and update version
    Added profile_template.yml file.
    Dremio adapater can be selected when using dbt init <project>
    but the smample profile cannot be found.
    
    Updated version of the adapter to be 1.2.2 but run into
    problems when using dbt --version.

--- setup.py
+++ setup.py
@@ -1,28 +1,21 @@
 #!/usr/bin/env python
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import find_namespace_packages, setup
 
 package_name = "dbt-dremio"
-package_version = "1.0.6.5"
-description = """The dremio adapter plugin for dbt (data build tool)"""
+# make sure this always matches dbt/adapters/{adapter}/__version__.py
+package_version = "1.2.2"
+description = """The DremioAdapter plugin for dbt"""
 
 setup(
     name=package_name,
     version=package_version,
     description=description,
     long_description=description,
-    author="Fabrice Etanchaud",
-    author_email="fabrice.etanchaud@netc.fr",
-    url="https://github.com/fabrice-etanchaud/dbt-dremio",
-    packages=find_packages(),
-    package_data={
-        'dbt': [
-            'include/dremio/macros/*.sql',
-            'include/dremio/macros/**/*.sql',
-            'include/dremio/macros/**/**/*.sql',
-            'include/dremio/dbt_project.yml',
-        ]
-    },
+    author="Dremio",
+    author_email="dremio@dremio.com",
+    url="https://github.com/dremio/dbt-dremio",
+    packages=find_namespace_packages(include=["dbt", "dbt.*"]),
+    include_package_data=True,
     install_requires=[
         'dbt-core~=1.2.0',
         'pyodbc>=4.0.27',
