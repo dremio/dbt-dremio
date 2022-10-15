58d6f6ed3bfbcdb44d112d17af8229df7a3699bb
Jared LaRue <jared.larue@dremio.com>
2022-10-15 18:39:54 +0000

    Corrected which license text needed updating

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     version=package_version,
     description=description,
     long_description=description,
-    license="Apache Software License",
+    license="Apache Software License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)",
     author="Dremio",
     url="https://github.com/dremio/dbt-dremio",
     packages=find_namespace_packages(include=["dbt", "dbt.*"]),
@@ -30,7 +30,7 @@ setup(
         'dbt-core==1.1.2',
     ],
     classifiers=[
-        "License :: OSI Approved :: Apache Software License 2.0",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python :: 3.10",
     ],
