164b2def8115ef874693d85fc684ccf7c6ba3c44
Ravjot Brar <ravjot.brar@dremio.com>
2022-10-21 15:22:08 -0700

    address comments and create test model

--- .github/scripts/smoke_test.sh
+++ .github/scripts/smoke_test.sh
@@ -1,13 +1,23 @@
 #! /bin/bash
 
-# Software test
-test_software() {
+# create test model
+create_model() {
+touch ./$project/models/select_model.sql
+{ cat > ./$project/models/select_model.sql << EOF
+SELECT *
+FROM Samples."samples.dremio.com"."zips.json" LIMIT 5
+EOF
+}
+}
+
+# software user/pw test
+test_softwareUP() {
 cd ../../.. && \
 if [ -d "./$project" ]; then
     rm -r $project
 fi
 dbt init -s $project && \
-{ cat > ./.dbt/profiles.yml << EOF
+{ cat > $profiles_path << EOF
 $project:
   outputs:
     dev:
@@ -16,16 +26,46 @@ $project:
       software_host: $test_host
       threads: 1
       type: dremio
-      use_ssl: false
+      use_ssl: $test_ssl
       user: $test_user
   target: dev
 EOF
 } && \
-
+echo 'vars:' >> ./$project/dbt_project.yml && \
+echo '  dremio:reflections_enabled: false' >> ./$project/dbt_project.yml && \
+create_model && \
 cd $project/ && \
 dbt debug && \
 dbt run
+}
 
+# software user/pat test
+test_softwarePAT() {
+cd ../../.. && \
+if [ -d "./$project" ]; then
+    rm -r $project
+fi
+dbt init -s $project && \
+{ cat > $profiles_path << EOF
+$project:
+  outputs:
+    dev:
+      pat: $test_token
+      port: 9047
+      software_host: $test_host
+      threads: 1
+      type: dremio
+      use_ssl: $test_ssl
+      user: $test_user
+  target: dev
+EOF
+} && \
+echo 'vars:' >> ./$project/dbt_project.yml && \
+echo '  dremio:reflections_enabled: false' >> ./$project/dbt_project.yml && \
+create_model && \
+cd $project/ && \
+dbt debug && \
+dbt run
 }
 
 #Cloud test
@@ -35,7 +75,7 @@ if [ -d "./$project" ]; then
     rm -r $project
 fi
 dbt init -s $project && \
-{ cat > ./.dbt/profiles.yml << EOF
+{ cat > $profiles_path << EOF
 $project:
   outputs:
     dev:
@@ -51,7 +91,7 @@ EOF
 } && \
 echo 'vars:' >> ./$project/dbt_project.yml && \
 echo '  dremio:reflections_enabled: false' >> ./$project/dbt_project.yml && \
-
+create_model && \
 cd $project/ && \
 dbt debug && \
 dbt run 
@@ -59,14 +99,26 @@ dbt run
 
 # Main
 test_type=$1
+test_ssl="${6:-false}"
+profiles_path=./.dbt/profiles.yml 
 
-if [ $test_type == software ]; then 
+if [ $test_type == softwareUP ]; then 
     test_user=$2
     test_password=$3
     test_host=$4
     test_port=$5
+  
     project=software_proj
-    test_software
+    test_softwareUP
+
+elif [ $test_type == softwarePAT ]; then
+    test_user=$2
+    test_token=$3
+    test_host=$4
+    test_port=$5
+    project=software_proj
+    test_softwarePAT
+
 elif [ $test_type == cloud ]; then
     test_user=$2
     test_token=$3
