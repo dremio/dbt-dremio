91f12f291246b76514902e5eab6467c6eb0a183e
Ravjot Brar <ravjot.brar@dremio.com>
2022-10-21 13:57:49 -0700

    add grant, incremental, and python model tests

---
+++ tests/functional/adapter/test_incremental_unique_id.py
@@ -0,0 +1,8 @@
+import pytest
+from dbt.tests.adapter.incremental.test_incremental_unique_id import (
+    BaseIncrementalUniqueKey,
+)
+
+
+class TestIncrementalUniqueKeyDremio(BaseIncrementalUniqueKey):
+    pass
