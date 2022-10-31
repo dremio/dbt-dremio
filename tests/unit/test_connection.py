2ec39519c5f97db6e02b36b650064bcd68556c96
ArgusLi <argus.li@dremio.com>
2022-10-30 18:15:15 -0700

    Create slightly too high up unit test.
    Create unit test that stubs the _post method and returns DremioRequestTimeoutException(). The reason why this unit test is slightly too high up is because it does not test the _check_error() and _raise_for_status() logic. Instead, it tests the dbt_core base adapter retry_connection logic, which is not what it should test.

--- tests/unit/test_connection.py
+++ tests/unit/test_connection.py
@@ -1,18 +1,27 @@
 from dbt.adapters.dremio.api.rest.error import DremioRequestTimeoutException
 import pytest
-from dbt.tests.util import run_dbt
 from unittest.mock import patch
 from dbt.adapters.dremio.connections import DremioConnectionManager
-
-from tests.unit.utils.fixtures import select_model_sql
+from requests.exceptions import HTTPError
+from dbt.exceptions import FailedToConnectException
 
 
 class TestRetryConnections:
-    @patch("dbt.adapters.dremio.api.handle.DremioHandle")
+    @patch("dbt.adapters.dremio.api.rest.endpoints._post")
     @patch("dbt.contracts.connection.Connection")
     # When you nest patch decorators the mocks are passed in to the decorated function in bottom up order.
-    def test_run_connection_retry_test(self, mocked_Connection, mocked_DremioHandle):
-        mocked_DremioHandle.cursor.side_effect = DremioRequestTimeoutException
+    def test_run_connection_retry_test(
+        self,
+        mocked_Connection,
+        mocked_post,
+    ):
 
-        DremioConnectionManager.open(connection=mocked_Connection)
-        mocked_DremioHandle.cursor.assert_called()
+        mocked_post.side_effect = DremioRequestTimeoutException(
+            msg="Request timeout: Test", original_exception="408 original exception"
+        )
+
+        with pytest.raises(FailedToConnectException) as exception_info:
+            DremioConnectionManager.open(connection=mocked_Connection)
+
+        # Post will be called once and then DEFAULT_TRIES more times.
+        assert mocked_post.call_count == DremioConnectionManager.DEFAULT_RETRIES + 1
