f1401dfd1e91147872f2c644eb0a8962820fcb01
ArgusLi <argus.li@dremio.com>
2022-11-01 12:30:08 -0700

    Resolve Comments

--- tests/unit/test_connection.py
+++ tests/unit/test_connection.py
@@ -1,9 +1,8 @@
-from dbt.adapters.dremio.api.rest.error import DremioRequestTimeoutException
 import pytest
 from unittest.mock import patch
-from dbt.adapters.dremio.connections import DremioConnectionManager
-from requests.exceptions import HTTPError
 from dbt.exceptions import FailedToConnectException
+from dbt.adapters.dremio.api.rest.error import DremioRequestTimeoutException
+from dbt.adapters.dremio.connections import DremioConnectionManager
 
 
 class TestRetryConnection:
@@ -12,19 +11,18 @@ class TestRetryConnection:
     # When you nest patch decorators the mocks are passed in to the decorated function in bottom up order.
     def test_connection_retry(
         self,
-        mocked_Connection,
-        mocked_post,
+        mocked_connection_obj,
+        mocked_post_func,
     ):
+        TOTAL_CONNECTION_ATTEMPTS = (
+            DremioConnectionManager.DEFAULT_CONNECTION_RETRIES + 1
+        )
 
-        mocked_post.side_effect = DremioRequestTimeoutException(
+        mocked_post_func.side_effect = DremioRequestTimeoutException(
             msg="Request timeout: Test", original_exception="408 original exception"
         )
 
         with pytest.raises(FailedToConnectException) as exception_info:
-            DremioConnectionManager.open(connection=mocked_Connection)
+            DremioConnectionManager.open(connection=mocked_connection_obj)
 
-        # Post will be called once and then DEFAULT_TRIES more times.
-        assert (
-            mocked_post.call_count
-            == DremioConnectionManager.DEFAULT_CONNECTION_RETRIES + 1
-        )
+        assert mocked_post_func.call_count == TOTAL_CONNECTION_ATTEMPTS
