e92f7ddfeecc4f8b032e28c7f60b4c3ab4d03601
ArgusLi <argus.li@dremio.com>
2022-11-01 12:45:10 -0700

    Add credentials information.

--- tests/unit/test_connection.py
+++ tests/unit/test_connection.py
@@ -1,6 +1,7 @@
 import pytest
 from unittest.mock import patch
 from dbt.exceptions import FailedToConnectException
+from dbt.adapters.dremio.credentials import DremioCredentials
 from dbt.adapters.dremio.api.rest.error import DremioRequestTimeoutException
 from dbt.adapters.dremio.connections import DremioConnectionManager
 
@@ -14,15 +15,23 @@ class TestRetryConnection:
         mocked_connection_obj,
         mocked_post_func,
     ):
+        # Arrange
         TOTAL_CONNECTION_ATTEMPTS = (
             DremioConnectionManager.DEFAULT_CONNECTION_RETRIES + 1
         )
+
+        mocked_connection_obj.credentials.software_host = ""
+        mocked_connection_obj.credentials.cloud_host = None
+        mocked_connection_obj.credentials.port = 9047
+        mocked_connection_obj.credentials.use_ssl = False
 
         mocked_post_func.side_effect = DremioRequestTimeoutException(
             msg="Request timeout: Test", original_exception="408 original exception"
         )
 
+        # Act
         with pytest.raises(FailedToConnectException) as exception_info:
             DremioConnectionManager.open(connection=mocked_connection_obj)
 
+        # Assert
         assert mocked_post_func.call_count == TOTAL_CONNECTION_ATTEMPTS
