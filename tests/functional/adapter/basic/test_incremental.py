385fd618dfe24b072f6c6f71670b0ae65378ba5c
ArgusLi <contactme@chunkeili.com>
2022-11-03 16:06:41 -0700

    Implement TestBaseIncrementalNotSchemaChange

--- tests/functional/adapter/basic/test_incremental.py
+++ tests/functional/adapter/basic/test_incremental.py
@@ -1,9 +1,40 @@
 import pytest
-from dbt.tests.adapter.basic.test_incremental import BaseIncremental
+from dbt.tests.adapter.basic.test_incremental import BaseIncremental, BaseIncrementalNotSchemaChange
 from tests.functional.adapter.utils.test_utils import DATALAKE
 
 
 class TestIncrementalDremio(BaseIncremental):
+    @pytest.fixture(scope="class")
+    def unique_schema(self, request, prefix) -> str:
+        test_file = request.module.__name__
+        # We only want the last part of the name
+        test_file = test_file.split(".")[-1]
+        unique_schema = f"{DATALAKE}.{prefix}_{test_file}"
+        return unique_schema
+
+    @pytest.fixture(scope="class")
+    def dbt_profile_data(
+        self, unique_schema, dbt_profile_target, profiles_config_update
+    ):
+        profile = {
+            "config": {"send_anonymous_usage_stats": False},
+            "test": {
+                "outputs": {
+                    "default": {},
+                },
+                "target": "default",
+            },
+        }
+        target = dbt_profile_target
+        target["schema"] = unique_schema
+        target["root_path"] = unique_schema
+        profile["test"]["outputs"]["default"] = target
+
+        if profiles_config_update:
+            profile.update(profiles_config_update)
+        return profile
+
+class TestBaseIncrementalNotSchemaChange(BaseIncrementalNotSchemaChange):
     @pytest.fixture(scope="class")
     def unique_schema(self, request, prefix) -> str:
         test_file = request.module.__name__
