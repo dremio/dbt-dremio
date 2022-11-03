01c3cd89e92738f33b9dbeeb78d34c06147db1f1
Jared LaRue <jared.larue@dremio.com>
2022-11-03 10:16:49 -0700

    Introduce renamed profile  parameters
    - object_storage_source <-> database
    - object_storage_path <-> schema
    - dremio_space <-> datalake
    - dremio_space_folder <-> root_path

--- dbt/adapters/dremio/credentials.py
+++ dbt/adapters/dremio/credentials.py
@@ -20,33 +20,39 @@ from dbt.adapters.dremio.relation import DremioRelation
 
 @dataclass
 class DremioCredentials(Credentials):
-    environment: Optional[str]
-    database: Optional[str]
-    schema: Optional[str]
-    datalake: Optional[str]
-    root_path: Optional[str]
+    environment: Optional[str] = None
+    UID: Optional[str] = None
+    PWD: Optional[str] = None
+    pat: Optional[str] = None
+    object_storage_source: Optional[str] = None
+    datalake: Optional[str] = None
+    object_storage_path: Optional[str] = None
+    root_path: Optional[str] = None
+    dremio_space: Optional[str] = None
+    database: Optional[str] = None
+    dremio_space_folder: Optional[str] = None
+    schema: Optional[str] = None
     cloud_project_id: Optional[str] = None
     cloud_host: Optional[str] = None
     software_host: Optional[str] = None
-    UID: Optional[str] = None
-    PWD: Optional[str] = None
     port: Optional[int] = 9047  # for rest endpoint
     use_ssl: Optional[bool] = True
-    pat: Optional[str] = None
     additional_parameters: Optional[str] = None
 
     _ALIASES = {
-        "user": "UID",
+        # Only terms on right-side will be used going forward.
         "username": "UID",
-        "pass": "PWD",
+        "user": "UID",  # backwards compatibility with existing profiles
         "password": "PWD",
-        "server": "host",
-        "track": "environment",
-        "space": "database",
-        "folder": "schema",
-        "materialization_database": "datalake",
-        "materialization_schema": "root_path",
+        "object_storage_source": "datalake",
+        "object_storage_path": "root_path",
+        "dremio_space": "database",
+        "dremio_space_folder": "schema",
+        "folder": "schema",  # backwards compatibility with existing profiles
     }
+
+    _DEFAULT_OBJECT_STORAGE_SOURCE = "$scratch"
+    _SPACE_NAME_PLACEHOLDER = "@user"
 
     @property
     def type(self):
@@ -58,22 +64,25 @@ class DremioCredentials(Credentials):
 
     def _connection_keys(self):
         # return an iterator of keys to pretty-print in 'dbt debug'
-        # raise NotImplementedError
-
         return (
-            "driver",
             "cloud_host",
             "cloud_project_id",
             "software_host",
             "port",
+            "username",
             "UID",
-            "database",
-            "schema",
-            "additional_parameters",
-            "datalake",
-            "root_path",
+            "additional_parameters",  # TODO: investigate
             "environment",
             "use_ssl",
+            "object_storage_source",
+            "space_root_folder",
+            "root_path",
+            "datalake",
+            "dremio_space",
+            "database",
+            "space_root_folder",
+            "schema",
+            "folder",
         )
 
     @classmethod
@@ -83,26 +92,31 @@ class DremioCredentials(Credentials):
             data["cloud_host"] = None
         if "software_host" not in data:
             data["software_host"] = None
+
         if "database" not in data:
             data["database"] = None
         if "schema" not in data:
             data["schema"] = None
+
         if "datalake" not in data:
             data["datalake"] = None
         if "root_path" not in data:
             data["root_path"] = None
-        if "environment" not in data:
-            data["environment"] = None
+
         if "pat" not in data:
             data["pat"] = None
+
+        if "environment" not in data:
+            data["environment"]
+
         return data
 
     def __post_init__(self):
-        if self.database is None:
-            self.database = "@" + self.UID
-        if self.schema is None:
-            self.schema = DremioRelation.no_schema
         if self.datalake is None:
-            self.datalake = "$scratch"
+            self.datalake = self._DEFAULT_OBJECT_STORAGE_SOURCE
         if self.root_path is None:
             self.root_path = DremioRelation.no_schema
+        if self.database is None or self.database == self._SPACE_NAME_PLACEHOLDER:
+            self.database = f"@{self.UID}"
+        if self.schema is None:
+            self.schema = DremioRelation.no_schema
