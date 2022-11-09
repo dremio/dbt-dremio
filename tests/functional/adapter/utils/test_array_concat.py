4b6ceeb919e391bbd07f61c23451dda4a093827c
Argus Li <43020525+ArgusLi@users.noreply.github.com>
2022-11-09 11:05:26 -0800

    Revert "dbt-dremio 1.3.0 Release Readiness" (#75)
    Reverts dremio/dbt-dremio#66

--- tests/functional/adapter/utils/test_array_concat.py
+++
@@ -1,8 +0,0 @@
-from dbt.tests.adapter.utils.test_array_concat import (
-    BaseArrayConcat,
-)
-from tests.fixtures.profiles import unique_schema, dbt_profile_data
-
-
-class TestArrayConcat(BaseArrayConcat):
-    pass
