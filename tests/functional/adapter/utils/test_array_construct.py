50ecd0b9a1da7112a73cfe29e2505d4bd0a37c1e
ArgusLi <contactme@chunkeili.com>
2022-11-09 11:04:41 -0800

    Fix tests that got changed by merge.

--- tests/functional/adapter/utils/test_array_construct.py
+++
@@ -1,8 +0,0 @@
-from dbt.tests.adapter.utils.test_array_construct import (
-    BaseArrayConstruct,
-)
-from tests.fixtures.profiles import unique_schema, dbt_profile_data
-
-
-class TestArrayConstruct(BaseArrayConstruct):
-    pass
