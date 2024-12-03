fb31c0e45d5d1548169b5b4e7d781f2aeff334ed
Bernardo Meireles <67381633+bcmeireles@users.noreply.github.com>
2024-12-03 23:31:34 +0000

    Update create_dbt_projects.sh

--- .github/scripts/create_dbt_projects.sh
+++ .github/scripts/create_dbt_projects.sh
@@ -3,34 +3,58 @@ set -e
 
 echo "Creating dbt projects..."
 
-# Function to initialize a dbt project
-init_dbt_project() {
-  local project_name=$1
-  local profile_selection=$2
-  local target_selection=$3
-  local host=$4
-  local username=$5
-  local password=$6
+dbt init test_cloud_options <<EOF
+1
+3
+localhost
 
-  dbt init "$project_name" <<EOF
-$profile_selection
-$target_selection
-$host
+dremio
+dremio123
 
-$username
-$password
+
+
+
+
 
 
 
 
 EOF
 
-  echo "dbt project '$project_name' created."
-}
+dbt init test_sw_up_options <<EOF
+1
+2
+localhost
 
-# Initialize each dbt project
-init_dbt_project "test_cloud_options" 1 3 "localhost" "dremio" "dremio123"
-init_dbt_project "test_sw_up_options" 1 2 "localhost" "dremio" "dremio123"
-init_dbt_project "test_sw_pat_options" 1 3 "localhost" "dremio" "dremio123"
+dremio
+dremio123
 
-echo "All dbt projects created successfully."
+
+
+
+
+
+
+
+
+EOF
+
+dbt init test_sw_pat_options <<EOF
+1
+3
+localhost
+
+dremio
+dremio123
+
+
+
+
+
+
+
+
+
+EOF
+
+          echo "projects created"
