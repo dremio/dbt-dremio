b7035be62580c6c173cddba816ca30f35b98fcf9
Arseniy Tsypushkin <arseniy.tsypushkin@dremio.com>
2025-01-03 16:19:41 +0000

    actual tests with existing functions

--- tests/functional/adapter/utils/test_array_concat.py
+++ tests/functional/adapter/utils/test_array_concat.py
@@ -14,8 +14,55 @@
 
 import pytest
 from dbt.tests.adapter.utils.test_array_concat import BaseArrayConcat
-from tests.fixtures.profiles import unique_schema, dbt_profile_data
+from tests.utils.util import BUCKET
+
+models__array_concat_expected_sql = """
+select 1 as id, array[1,2,3,4,5,6] as array_col union all
+select 2 as id, array[2,3] as array_col union all
+select 3 as id, array[3,2] as array_col
+"""
+
+models__array_concat_actual_sql = """
+select 1 as id, array_concat(array[1,2,3], array[4,5,6]) as array_col union all
+select 2 as id, array_concat(array[2], array[3]) as array_col union all
+select 3 as id, array_concat(array[3], array[2]) as array_col
+"""
 
 
 class TestArrayConcat(BaseArrayConcat):
-    pass
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "actual.sql": models__array_concat_actual_sql,
+            "expected.sql": models__array_concat_expected_sql,
+        }
+
+    # Override this fixture to prepend our schema with BUCKET
+    # This ensures the schema works with our datalake
+    @pytest.fixture(scope="class")
+    def unique_schema(self, request, prefix) -> str:
+        test_file = request.module.__name__
+        test_file = test_file.split(".")[-1]
+        unique_schema = f"{BUCKET}.{prefix}_{test_file}"
+        return unique_schema
+
+    @pytest.fixture(scope="class")
+    def dbt_profile_data(
+        self, unique_schema, dbt_profile_target, profiles_config_update
+    ):
+        profile = {
+            "test": {
+                "outputs": {
+                    "default": {},
+                },
+                "target": "default",
+            },
+        }
+        target = dbt_profile_target
+        target["schema"] = unique_schema
+        target["root_path"] = unique_schema
+        profile["test"]["outputs"]["default"] = target
+
+        if profiles_config_update:
+            profile.update(profiles_config_update)
+        return profile
