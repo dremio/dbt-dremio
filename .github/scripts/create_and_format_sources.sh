3085a9622b02da8085aa8d14d0812a5de24bf038
Liliana Zheng <liliana.zheng@dremio.com>
2025-03-03 13:53:45 +0000

    Pipeline scripts adjustments

--- .github/scripts/create_and_format_sources.sh
+++ .github/scripts/create_and_format_sources.sh
@@ -9,8 +9,6 @@ set -e
 : "${MINIO_ROOT_USER:?Need to set MINIO_ROOT_USER}"
 : "${MINIO_ROOT_PASSWORD:?Need to set MINIO_ROOT_PASSWORD}"
 
-echo "Creating Dremio S3 Source..."
-
 for i in $(seq 1 $RETRY_COUNT); do
   if curl -s $DREMIO_HEALTH_URL; then
     echo "Dremio is up."
@@ -27,7 +25,7 @@ fi
 
 # Obtain Dremio auth token
 echo "Logging into Dremio to obtain auth token..."
-AUTH_RESPONSE=$(curl -s -X POST "http://localhost:9047/apiv2/login" \
+AUTH_RESPONSE=$(curl -s -X POST "$DREMIO_HEALTH_URL/apiv2/login" \
   -H "Content-Type: application/json" \
   --data "{\"userName\":\"${DREMIO_SOFTWARE_USERNAME}\", \"password\":\"${DREMIO_SOFTWARE_PASSWORD}\"}")
 
@@ -41,37 +39,108 @@ fi
 
 echo "Obtained Dremio auth token."
 echo "::add-mask::$AUTH_TOKEN"
-echo "AUTH_TOKEN=${AUTH_TOKEN}" >> $GITHUB_ENV
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+  echo "Running in GitHub Actions"
+  echo "AUTH_TOKEN=${AUTH_TOKEN}" >> $GITHUB_ENV
+else # Jenkins
+  echo $AUTH_TOKEN > /tmp/auth_token.txt
+fi
 
-# Create the S3 source in Dremio
-echo "Creating the S3 source in Dremio..."
-curl -s -X PUT "http://localhost:9047/apiv2/source/dbt_test_source" \
-  -H "Content-Type: application/json" \
-  -H "Authorization: _dremio$AUTH_TOKEN" \
-  --data "{\"name\":\"dbt_test_source\",\"config\":{\"credentialType\":\"ACCESS_KEY\",\"accessKey\":\"$MINIO_ROOT_USER\",\"accessSecret\":\"$MINIO_ROOT_PASSWORD\",\"secure\":false,\"externalBucketList\":[],\"enableAsync\":true,\"enableFileStatusCheck\":true,\"rootPath\":\"/\",\"defaultCtasFormat\":\"ICEBERG\",\"propertyList\":[{\"name\":\"fs.s3a.path.style.access\",\"value\":\"true\"},{\"name\":\"fs.s3a.endpoint\",\"value\":\"minio:9000\"},{\"name\":\"dremio.s3.compat\",\"value\":\"true\"}],\"whitelistedBuckets\":[],\"isCachingEnabled\":false,\"maxCacheSpacePct\":100},\"type\":\"S3\",\"metadataPolicy\":{\"deleteUnavailableDatasets\":true,\"autoPromoteDatasets\":false,\"namesRefreshMillis\":3600000,\"datasetDefinitionRefreshAfterMillis\":3600000,\"datasetDefinitionExpireAfterMillis\":10800000,\"authTTLMillis\":86400000,\"updateMode\":\"PREFETCH_QUERIED\"}}"
+manipulate_source() {
+  local url=$1
+  local data=$2
+  local description=$3
+  local raw=$4
 
-echo "S3 Source created in Dremio."
+  echo "$description in Dremio..."
 
-echo "Creating the Samples source in Dremio..."
-curl -s -X PUT  "http://localhost:9047/apiv2/source/Samples" \
-  -H "Content-Type: application/json" \
-  -H "Authorization: _dremio$AUTH_TOKEN" \
-  --data-raw "{\"name\":\"Samples\",\"config\":{\"externalBucketList\":[\"samples.dremio.com\"],\"credentialType\":\"NONE\",\"secure\":false,\"propertyList\":[]},\"name\":\"Samples\",\"accelerationRefreshPeriod\":3600000,\"accelerationGracePeriod\":10800000,\"accelerationNeverRefresh\":true,\"accelerationNeverExpire\":true,\"accelerationActivePolicyType\":\"PERIOD\",\"accelerationRefreshSchedule\":\"0 0 8 * * *\",\"accelerationRefreshOnDataChanges\":false,\"type\":\"S3\"}"
+  if [ "$raw" == "true" ]; then
+    RESPONSE=$(curl --show-error -s -w "\n%{http_code}" -X PUT "$url" \
+      -H "Content-Type: application/json" \
+      -H "Authorization: _dremio$AUTH_TOKEN" \
+      --data-raw "$data")
+  else
+    RESPONSE=$(curl --show-error -s -w "\n%{http_code}" -X PUT "$url" \
+      -H "Content-Type: application/json" \
+      -H "Authorization: _dremio$AUTH_TOKEN" \
+      --data "$data")
+  fi
 
-echo "Samples source created in Dremio."
+  HTTP_STATUS=$(echo "$RESPONSE" | tail -n1)
+  RESPONSE_BODY=$(echo "$RESPONSE" | sed '$d')
 
-echo "Formatting SF_incidents2016..."
-curl -s -X PUT "http://localhost:9047/apiv2/source/Samples/file_format/samples.dremio.com/SF_incidents2016.json" \
-  -H "Content-Type: application/json" \
-  -H "Authorization: _dremio$AUTH_TOKEN" \
-  --data-raw "{\"type\":\"JSON\"}"
+  if [ "$HTTP_STATUS" -eq 200 ]; then
+    echo -e "\n$description successfull in Dremio."
+  else
+    echo -e "\n$description failed: $RESPONSE_BODY"
+    exit 1
+  fi
+}
 
-echo "SF_incidents2016 formatted in Dremio."
+manipulate_source "$DREMIO_HEALTH_URL/apiv2/source/dbt_test_source" \
+  "{
+    \"name\":\"dbt_test_source\",
+    \"config\":{
+      \"credentialType\":\"ACCESS_KEY\",
+      \"accessKey\":\"$MINIO_ROOT_USER\",
+      \"accessSecret\":\"$MINIO_ROOT_PASSWORD\",
+      \"secure\":false,
+      \"externalBucketList\":[],
+      \"enableAsync\":true,
+      \"enableFileStatusCheck\":true,
+      \"rootPath\":\"/\",
+      \"defaultCtasFormat\":\"ICEBERG\",
+      \"propertyList\":[
+        {\"name\":\"fs.s3a.path.style.access\",\"value\":\"true\"},
+        {\"name\":\"fs.s3a.endpoint\",\"value\":\"minio:9000\"},
+        {\"name\":\"dremio.s3.compat\",\"value\":\"true\"}
+      ],
+      \"whitelistedBuckets\":[],
+      \"isCachingEnabled\":false,
+      \"maxCacheSpacePct\":100
+    },
+    \"type\":\"S3\",
+    \"metadataPolicy\":{
+      \"deleteUnavailableDatasets\":true,
+      \"autoPromoteDatasets\":false,
+      \"namesRefreshMillis\":3600000,
+      \"datasetDefinitionRefreshAfterMillis\":3600000,
+      \"datasetDefinitionExpireAfterMillis\":10800000,
+      \"authTTLMillis\":86400000,
+      \"updateMode\":\"PREFETCH_QUERIED\"
+    }
+  }" \
+  "Creating S3 source" \
+  "false"
 
-echo "Formatting NYC-taxi-trips..."
-curl -s -X PUT "http://localhost:9047/apiv2/source/Samples/folder_format/samples.dremio.com/NYC-taxi-trips" \
-  -H "Content-Type: application/json" \
-  -H "Authorization: _dremio$AUTH_TOKEN" \
-  --data-raw "{\"ignoreOtherFileFormats\":false,\"type\":\"Parquet\"}"
+manipulate_source "$DREMIO_HEALTH_URL/apiv2/source/Samples" \
+  "{
+    \"name\":\"Samples\",
+    \"config\":{
+      \"externalBucketList\":[\"samples.dremio.com\"],
+      \"credentialType\":\"NONE\",
+      \"secure\":false,
+      \"propertyList\":[]
+    },
+    \"accelerationRefreshPeriod\":3600000,
+    \"accelerationGracePeriod\":10800000,
+    \"accelerationNeverRefresh\":true,
+    \"accelerationNeverExpire\":true,
+    \"accelerationActivePolicyType\":\"PERIOD\",
+    \"accelerationRefreshSchedule\":\"0 0 8 * * *\",
+    \"accelerationRefreshOnDataChanges\":false,
+    \"type\":\"S3\"
+  }" \
+  "Creating Samples source" \
+  "true"
 
-echo "NYC-taxi-trips formatted in Dremio."
+manipulate_source "$DREMIO_HEALTH_URL/apiv2/source/Samples/file_format/samples.dremio.com/SF_incidents2016.json" \
+  "{\"type\":\"JSON\"}" \
+  "Formatting SF_incidents2016" \
+  "true"
+
+manipulate_source "$DREMIO_HEALTH_URL/apiv2/source/Samples/folder_format/samples.dremio.com/NYC-taxi-trips" \
+  "{\"ignoreOtherFileFormats\":false,\"type\":\"Parquet\"}" \
+  "Formatting NYC-taxi-trips" \
+  "true"
+
