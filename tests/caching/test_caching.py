80fcec8c7e71a97289880e478c0409b0533e319c
Simon Pannek <simon.pannek@dremio.com>
2025-03-10 17:17:54 +0000

    Minor changes

--- tests/caching/test_caching.py
+++ tests/caching/test_caching.py
@@ -8,6 +8,38 @@ from dbt.tests.adapter.caching.test_caching import (
     model_sql,
 )
 
+
+# This ensures the schema works with our datalake
+@pytest.fixture(scope="class")
+def unique_schema(request, prefix) -> str:
+    test_file = request.module.__name__
+    # We only want the last part of the name
+    test_file = test_file.split(".")[-1]
+    unique_schema = f"{BUCKET}.{prefix}_{test_file}"
+    return unique_schema
+
+
+# Override this fixture to set root_path=schema
+@pytest.fixture(scope="class")
+def dbt_profile_data(unique_schema, dbt_profile_target, profiles_config_update):
+    profile = {
+        "test": {
+            "outputs": {
+                "default": {},
+            },
+            "target": "default",
+        },
+    }
+    target = dbt_profile_target
+    target["schema"] = unique_schema
+    target["root_path"] = f"{unique_schema}"
+    profile["test"]["outputs"]["default"] = target
+
+    if profiles_config_update:
+        profile.update(profiles_config_update)
+    return profile
+
+
 class TestNoPopulateCacheDremio(TestNoPopulateCache):
     pass
 
