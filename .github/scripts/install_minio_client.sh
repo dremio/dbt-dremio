65ec7e966ac28f7e6769106634ad27127379aeb7
Liliana Zheng <liliana.zheng@dremio.com>
2025-04-01 13:59:23 +0100

    Adjust existing pipeline scripts to support Jenkins (#283)
    ### Summary
    Adjust and reformat existing pipeline scripts to support Jenkins and
    have more legible and accurate testing results when running dbt against
    Dremio EE pipelines
    
    ### Description
    
    - Created variables and functions to avoid code repetition
    - Handled error status
    - Included files (*profiles.yml* and *projects.zip*) for the dbt test
    projects creation, instead of using `dbt init` command
    
    ### Test Results
    
    All tests are passing, no new tests were added
    
    ### Changelog
    
    -   [x] Added a summary of what this PR accomplishes to CHANGELOG.md

--- .github/scripts/install_minio_client.sh
+++ .github/scripts/install_minio_client.sh
@@ -3,8 +3,11 @@ set -e
 
 echo "Installing MinIO Client (mc)..."
 
-curl -O https://dl.min.io/client/mc/release/linux-amd64/mc
-chmod +x mc
-sudo mv mc /usr/local/bin/
-
-echo "MinIO Client installed successfully."
+{
+    curl -O https://dl.min.io/client/mc/release/linux-amd64/mc &&
+    chmod +x mc &&
+    mv mc /usr/local/bin/
+} && echo "MinIO Client installed successfully." || {
+    echo "Failed to install MinIO Client."
+    exit 1
+}
