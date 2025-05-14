2aa320c7efcdc4f4ab797ea2a46dd6279086494e
Liliana Zheng <liliana.zheng@dremio.com>
2025-05-14 17:59:58 +0100

    Ensure case insensitive check

--- dbt/include/dremio/macros/utils/safe_cast.sql
+++ dbt/include/dremio/macros/utils/safe_cast.sql
@@ -1,5 +1,5 @@
 {% macro dremio__safe_cast(field, type) %}
-    {% if type.startswith('interval') or (field|string).startswith("'convert_from") %}
+    {% if type.lower().startswith('interval') or (field|string).lower().startswith("'convert_from") %}
         {{field}}
     {% elif type == 'array' %}
         {{type ~ field}}
