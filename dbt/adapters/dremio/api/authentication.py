e134ac614f86f3c88c3f216dfb94823ce137bd02
Simon Pannek <simon.pannek@dremio.com>
2025-05-23 16:55:32 +0100

    Minor changes

--- dbt/adapters/dremio/api/authentication.py
+++ dbt/adapters/dremio/api/authentication.py
@@ -41,7 +41,6 @@ class DremioAuthentication:
     def build_headers(cls, authorization_field):
         headers = {
             "Content-Type": "application/json",
-            "User-Agent": "Dremio QA DCS automation",
             "Authorization": "{authorization_token}".format(
                 authorization_token=authorization_field
             ),
